8bbad0c167d62516a0c75f06363a9a6e3b3c7088
Ryan Zhu <179405987@qq.com>
2013-06-23 08:55:26 +0800

    make root path and publish the admin interface

--- config/routes.rb
+++ config/routes.rb
@@ -65,7 +65,7 @@ RyanBlog::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'welcome#index'
+  root :to => 'articles#index'
 
   # See how all your routes lay out with "rake routes"
 
