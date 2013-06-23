a580f11d2211bd9cf116d80635f4f252b132eba6
Ryan Zhu <179405987@qq.com>
2013-06-23 09:46:39 +0800

    change article_id in tags to integer

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130620051815) do
+ActiveRecord::Schema.define(:version => 20130623014457) do
 
   create_table "admins", :force => true do |t|
     t.string   "name"
@@ -48,7 +48,7 @@ ActiveRecord::Schema.define(:version => 20130620051815) do
     t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
-    t.string   "article_id"
+    t.integer  "article_id"
   end
 
 end
