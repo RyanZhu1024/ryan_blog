b6c5f0f5d7111d3c68bbd48fa14ab943d0a75ea1
Ryan Zhu <179405987@qq.com>
2013-06-23 09:16:23 +0800

    remove tag_article_relationship migration file

--- db/migrate/20130608093454_remove_table_tag_article_relationship.rb
+++
@@ -1,9 +0,0 @@
-class RemoveTableTagArticleRelationship < ActiveRecord::Migration
-  def up
-  	drop_table :tag_article_relationships
-  end
-
-  def down
-  	create_table :tag_article_relationships
-  end
-end
